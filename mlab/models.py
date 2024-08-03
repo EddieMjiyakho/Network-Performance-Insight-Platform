10a300070818f45db9aa115caee019dd20644dcb
Lehlohonolo Mosikili <karabolehlohonolo242@gmail.com>
2024-08-03 20:06:50 +0200

    Connect postgreSQL database

--- mlab/models.py
+++ mlab/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class MLabData(models.Model):
-    test_id = models.CharField(max_length=255)
     download_throughput = models.FloatField()
     upload_throughput = models.FloatField()
     latency = models.FloatField()
