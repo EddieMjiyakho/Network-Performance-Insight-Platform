89f4c302a2c073df7ec7627553faed56536fd36c
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2024-08-12 19:06:51 +0200

    add fields clientCountry and clientCity in models.py

--- mlab/models.py
+++ mlab/models.py
@@ -24,3 +24,14 @@ class NetworkPerformance(models.Model):
     avg_download_speed = models.FloatField()
     avg_upload_speed = models.FloatField()
     avg_latency = models.FloatField()
+
+class NetworkPerformanceData(models.Model):
+    date = models.DateField()
+    clientCountry = models.CharField(max_length=255)
+    clientCity = models.CharField(max_length=255)
+    clientRegion = models.CharField(max_length=255)
+    clientASN = models.CharField(max_length=255)
+    avg_download_speed = models.FloatField()
+    avg_upload_speed = models.FloatField()
+    avg_latency = models.FloatField()
+
