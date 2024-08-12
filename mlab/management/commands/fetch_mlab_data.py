db4af7292de279b4962ed77992163267d1c88549
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2024-08-12 19:02:58 +0200

    update the query to fetch other fields clientCity and clientRegion

--- mlab/management/commands/fetch_mlab_data.py
+++ mlab/management/commands/fetch_mlab_data.py
@@ -10,44 +10,53 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         query = """
             SELECT
-            date,
-            clientCountry,
-            clientASN,
-            -- Calculate average for download.bps array and round to 2 decimal places
-            ROUND(
-                (
-                SELECT
-                    AVG(value)
-                FROM
-                    UNNEST(download.bps) AS value
-                ), 2
-            ) AS avg_download_speed,
-            -- Calculate average for upload.bps array and round to 2 decimal places
-            ROUND(
-                (
-                SELECT
-                    AVG(value)
-                FROM
-                    UNNEST(upload.bps) AS value
-                ), 2
-            ) AS avg_upload_speed,
-            -- Calculate average for latencyMs array and round to 2 decimal places
-            ROUND(
-                (
-                SELECT
-                    AVG(value)
-                FROM
-                    UNNEST(latencyMs) AS value
-                ), 2
-            ) AS avg_latency
+                date,
+                clientCountry,
+                clientCity,
+                clientRegion,
+                clientASN,
+                -- Calculate average for download.bps array and round to 2 decimal places
+                ROUND(
+                    (
+                    SELECT
+                        AVG(value)
+                    FROM
+                        UNNEST(download.bps) AS value
+                    ), 2
+                ) AS avg_download_speed,
+                -- Calculate average for upload.bps array and round to 2 decimal places
+                ROUND(
+                    (
+                    SELECT
+                        AVG(value)
+                    FROM
+                        UNNEST(upload.bps) AS value
+                    ), 2
+                ) AS avg_upload_speed,
+                -- Calculate average for latencyMs array and round to 2 decimal places
+                ROUND(
+                    (
+                    SELECT
+                        AVG(value)
+                    FROM
+                        UNNEST(latencyMs) AS value
+                    ), 2
+                ) AS avg_latency
             FROM
-            `measurement-lab.cloudflare.speedtest_speed1`
+                `measurement-lab.cloudflare.speedtest_speed1`
             WHERE
-            clientCountry IN ('AD', 'AO', 'BJ', 'BW', 'BF', 'BI', 'CM', 'CV', 'CF', 'TD', 'KM', 'CG', 'CD', 'DJ', 'EG', 'GQ', 'ER', 'SZ', 'ET', 'GA', 'GM', 'GH', 'GN', 'GW', 'CI', 'KE', 'LS', 'LR', 'LY', 'MG', 'MW', 'ML', 'MR', 'MU', 'MA', 'MZ', 'NA', 'NE', 'NG', 'RW', 'SH', 'ST', 'SN', 'SC', 'SL', 'SO', 'ZA', 'SS', 'SD', 'TZ', 'TG', 'TN', 'UG', 'ZM', 'ZW')
-            AND date >= '2023-01-01' AND date <= '2023-12-31'
+                clientCountry IN ('AD', 'AO', 'BJ', 'BW', 'BF', 'BI', 'CM', 'CV', 'CF', 'TD', 'KM', 'CG', 'CD', 'DJ', 'EG', 'GQ', 'ER', 'SZ', 'ET', 'GA', 'GM', 'GH', 'GN', 'GW', 'CI', 'KE', 'LS', 'LR', 'LY', 'MG', 'MW', 'ML', 'MR', 'MU', 'MA', 'MZ', 'NA', 'NE', 'NG', 'RW', 'SH', 'ST', 'SN', 'SC', 'SL', 'SO', 'ZA', 'SS', 'SD', 'TZ', 'TG', 'TN', 'UG', 'ZM', 'ZW')
+                AND date >= '2023-08-01' AND date <= '2024-08-12'
+                AND clientCountry IS NOT NULL AND clientCountry != ''
+                AND clientRegion IS NOT NULL AND clientRegion != ''
+                AND clientCity IS NOT NULL AND clientCity != ''
+                AND clientASN IS NOT NULL AND clientASN > 0
+                AND ARRAY_LENGTH(download.bps) > 0
+                AND ARRAY_LENGTH(upload.bps) > 0
+                AND ARRAY_LENGTH(latencyMs) > 0
             ORDER BY
-            date ASC
-            LIMIT 100;
+                date ASC
+            LIMIT 500000;
         """
 
         client = bigquery.Client()
