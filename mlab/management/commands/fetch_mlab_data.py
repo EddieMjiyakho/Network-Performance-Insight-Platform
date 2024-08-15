ac89829a915ab1b81863c77e548dd551e2fa6ff9
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2024-08-15 22:49:06 +0200

    update the SQL query to group data by date, country, city, region and ASN

--- mlab/management/commands/fetch_mlab_data.py
+++ mlab/management/commands/fetch_mlab_data.py
@@ -15,48 +15,58 @@ class Command(BaseCommand):
                 clientCity,
                 clientRegion,
                 clientASN,
-                -- Calculate average for download.bps array and round to 2 decimal places
-                ROUND(
-                    (
-                    SELECT
-                        AVG(value)
-                    FROM
-                        UNNEST(download.bps) AS value
-                    ), 2
-                ) AS avg_download_speed,
-                -- Calculate average for upload.bps array and round to 2 decimal places
-                ROUND(
-                    (
-                    SELECT
-                        AVG(value)
-                    FROM
-                        UNNEST(upload.bps) AS value
-                    ), 2
-                ) AS avg_upload_speed,
-                -- Calculate average for latencyMs array and round to 2 decimal places
-                ROUND(
-                    (
-                    SELECT
-                        AVG(value)
-                    FROM
-                        UNNEST(latencyMs) AS value
-                    ), 2
-                ) AS avg_latency
-            FROM
-                `measurement-lab.cloudflare.speedtest_speed1`
-            WHERE
-                clientCountry IN ('AD', 'AO', 'BJ', 'BW', 'BF', 'BI', 'CM', 'CV', 'CF', 'TD', 'KM', 'CG', 'CD', 'DJ', 'EG', 'GQ', 'ER', 'SZ', 'ET', 'GA', 'GM', 'GH', 'GN', 'GW', 'CI', 'KE', 'LS', 'LR', 'LY', 'MG', 'MW', 'ML', 'MR', 'MU', 'MA', 'MZ', 'NA', 'NE', 'NG', 'RW', 'SH', 'ST', 'SN', 'SC', 'SL', 'SO', 'ZA', 'SS', 'SD', 'TZ', 'TG', 'TN', 'UG', 'ZM', 'ZW')
-                AND date >= '2023-08-01'
-                AND clientCountry IS NOT NULL AND clientCountry != ''
-                AND clientRegion IS NOT NULL AND clientRegion != ''
-                AND clientCity IS NOT NULL AND clientCity != ''
-                AND clientASN IS NOT NULL AND clientASN > 0
-                AND ARRAY_LENGTH(download.bps) > 0
-                AND ARRAY_LENGTH(upload.bps) > 0
-                AND ARRAY_LENGTH(latencyMs) > 0
-            ORDER BY
-                date ASC
-            LIMIT 300000;
+                ROUND(AVG(download_speed), 2) AS avg_download_speed,
+                ROUND(AVG(upload_speed), 2) AS avg_upload_speed,
+                ROUND(AVG(latency), 2) AS avg_latency
+            FROM (
+                SELECT
+                    date,
+                    clientCountry,
+                    clientCity,
+                    clientRegion,
+                    clientASN,
+                    ROUND(
+                        (
+                        SELECT
+                            AVG(value)
+                        FROM
+                            UNNEST(download.bps) AS value
+                        ), 2
+                    ) AS download_speed,
+                    ROUND(
+                        (
+                        SELECT
+                            AVG(value)
+                        FROM
+                            UNNEST(upload.bps) AS value
+                        ), 2
+                    ) AS upload_speed,
+                    ROUND(
+                        (
+                        SELECT
+                            AVG(value)
+                        FROM
+                            UNNEST(latencyMs) AS value
+                        ), 2
+                    ) AS latency
+                FROM
+                    `measurement-lab.cloudflare.speedtest_speed1`
+                WHERE
+                    clientCountry IN ('AD', 'AO', 'BJ', 'BW', 'BF', 'BI', 'CM', 'CV', 'CF', 'TD', 'KM', 'CG', 'CD', 'DJ', 'EG', 'GQ', 'ER', 'SZ', 'ET', 'GA', 'GM', 'GH', 'GN', 'GW', 'CI', 'KE', 'LS', 'LR', 'LY', 'MG', 'MW', 'ML', 'MR', 'MU', 'MA', 'MZ', 'NA', 'NE', 'NG', 'RW', 'SH', 'ST', 'SN', 'SC', 'SL', 'SO', 'ZA', 'SS', 'SD', 'TZ', 'TG', 'TN', 'UG', 'ZM', 'ZW')
+                    AND date >= '2023-08-01'
+                    AND clientCountry IS NOT NULL AND clientCountry != ''
+                    AND clientRegion IS NOT NULL AND clientRegion != ''
+                    AND clientCity IS NOT NULL AND clientCity != ''
+                    AND clientASN IS NOT NULL AND clientASN > 0
+                    AND ARRAY_LENGTH(download.bps) > 0
+                    AND ARRAY_LENGTH(upload.bps) > 0
+                    AND ARRAY_LENGTH(latencyMs) > 0
+                ORDER BY
+                    date ASC
+                LIMIT 100000;
+            )
+            GROUP BY
+                date, clientCountry, clientCity, clientRegion, clientASN
         """
 
         client = bigquery.Client()
