023bc8323b9914390edaad1a25d82056cee9c3e8
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2024-08-29 16:38:52 +0200

    add network data tables to models.py

--- mlab/models.py
+++ mlab/models.py
@@ -1,30 +1,5 @@
 from django.db import models
 
-class MLabData(models.Model):
-    download_throughput = models.FloatField()
-    upload_throughput = models.FloatField()
-    latency = models.FloatField()
-
-    def __str__(self):
-        return self.test_id
-    
-class RipeAtlasData(models.Model):
-    probe_id = models.IntegerField()
-    measurement_id = models.IntegerField()
-    result = models.JSONField()
-    timestamp = models.DateTimeField()
-
-    def __str__(self):
-        return f"Probe ID: {self.probe_id}, Measurement ID: {self.measurement_id}"
-
-class NetworkPerformance(models.Model):
-    date = models.DateField()
-    clientCountry = models.CharField(max_length=255)
-    clientASN = models.CharField(max_length=255)
-    avg_download_speed = models.FloatField()
-    avg_upload_speed = models.FloatField()
-    avg_latency = models.FloatField()
-
 class NetworkPerformanceData(models.Model):
     date = models.DateField()
     clientCountry = models.CharField(max_length=255)
