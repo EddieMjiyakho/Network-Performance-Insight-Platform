80cbaec8abc0e0030c650b52c5110be3f85194c3
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2024-08-30 13:56:21 +0200

    add javascript and css static files

--- mlab/urls.py
+++ mlab/urls.py
@@ -1,12 +1,6 @@
 from . import views
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-# from .views import MyModelViewSet
-
-# router = DefaultRouter()
-# router.register(r'mymodel', MyModelViewSet)
 
 urlpatterns = [
-    # path('api/', include(router.urls)),
-    path('metrics/', views.show_Metrics, name='metrics'),
+    path('', views.index, name='index'),
 ]
--- mlab/views.py
+++ mlab/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-# from .models import MyModel
-# from .serializers import MyModelSerializer
 from .entities import *
 from mlab.models import NetworkPerformanceData  # 
 from django.core.management.base import BaseCommand
 
 metrics = NetworkPerformanceData.objects.all()  # object for the db table
 
-
-# class MyModelViewSet(viewsets.ModelViewSet):
-#     queryset = MyModel.objects.all()
-#     serializer_class = MyModelSerializer
-
 countries = []
 
-def show_Metrics(request):
-     for metric in metrics:
-        if metric.clientCountry == 'ZA':
-            metric = NetworkMetric(id=1, metric_name='metric', value=50.0, timestamp=metric.date, country_id=metric.clientCountry, region_id=metric.clientRegion, city_id=metric.clientCity)
-            countries.append(metric)
-
-     context = {'metrics': countries}
-     return render(request, 'index.html', context)
+def index(request):
+    return render(request, 'index.html')
