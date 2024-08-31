2abbf8e535389cb8b3596b016b5dbeeb36435d8c
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2024-09-01 00:28:27 +0200

    print all number of total records

--- mlab/management/commands/Print_Metrics.py
+++ mlab/management/commands/Print_Metrics.py
@@ -1,15 +1,20 @@
 from django.core.management.base import BaseCommand
-from mlab.models import NetworkPerformanceData  # 
+from mlab.models import NetworkPerformanceData
 
 class Command(BaseCommand):
     help = 'Print all network performance metrics'
 
     def handle(self, *args, **kwargs):
         metrics = NetworkPerformanceData.objects.all()
+        total_records = metrics.count()
+        
         for metric in metrics:
             self.stdout.write(
                 f"Date: {metric.date}, Download Speed: {metric.avg_download_speed}, "
                 f"Upload Speed: {metric.avg_upload_speed}, Latency: {metric.avg_latency}, "
-                f"Upload Speed: {metric.clientCity}, Latency: {metric.clientCity}, "
+                f"Client City: {metric.clientCity}, Client Region: {metric.clientRegion}, "
                 f"Client ASN: {metric.clientASN}, Client Country: {metric.clientCountry}"
             )
+        
+        # Print the total number of records at the end
+        self.stdout.write(f"\nTotal number of records: {total_records}")
