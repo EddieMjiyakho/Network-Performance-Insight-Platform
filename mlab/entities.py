96182d4009904cd9b3cff0a1600b68318c85493b
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2024-09-03 15:01:22 +0200

    edit entities.py to match the structure of the database

--- mlab/entities.py
+++ mlab/entities.py
@@ -1,143 +1,158 @@
-# @author: Maphuti Shilabje
-# email: fabridgeshilabje01@gmail.com
-# Date: 14/08/2024
-# The class to store istances from the Database
-
-# class User:   # This class represents a user in the system.
-#     def __init__(self, id, username, email, password, role):
-#         self.id = id
-#         self.username = username
-#         self.email = email
-#         self.password = password
-#         self.role = role
-
-#     def get_id(self):
-#         return self.id
-
-#     def get_username(self):
-#         return self.username
-
-#     def get_email(self):
-#         return self.email
-
-#     def set_username(self, username):
-#         self.username = username
-
-#     def set_email(self, email):
-#         self.email = email
-
-
-class Country:   # This class represents a country, which has associated ISPs
-    def __init__(self, id, country_name, ISPs):
+class Country:   # Represents a country with associated ISPs
+    def __init__(self, id, name):
         self.id = id
-        self.country_name = country_name
-        self.ISPs = ISPs
+        self.name = name
 
     def get_id(self):
         return self.id
 
-    def get_country_name(self):
-        return self.country_name
+    def get_name(self):
+        return self.name
 
-    def get_ISPs(self):
-        return self.ISPs
-
-    def set_country_name(self, country_name):
-        self.country_name = country_name
-
-    def set_ISPs(self, ISPs):
-        self.ISPs = ISPs
+    def set_name(self, name):
+        self.name = name
 
 
-class Region:   # This class represents a region within a country, also associated with ISPs.
-    def __init__(self, id, region_name, ISPs):
+class Region:   # Represents a region within a country
+    def __init__(self, id, name, country_id):
         self.id = id
-        self.region_name = region_name
-        self.ISPs = ISPs
-
-    def get_id(self):
-        return self.id
-
-    def get_region_name(self):
-        return self.region_name
-
-    def get_ISPs(self):
-        return self.ISPs
-
-    def set_region_name(self, region_name):
-        self.region_name = region_name
-
-    def set_ISPs(self, ISPs):
-        self.ISPs = ISPs
-
-
-class City:     # This class represents a region within a country, also associated with ISPs.
-    def __init__(self, id, city_name, ISPs):
-        self.id = id
-        self.city_name = city_name
-        self.ISPs = ISPs
-
-    def get_id(self):
-        return self.id
-
-    def get_city_name(self):
-        return self.city_name
-
-    def get_ISPs(self):
-        return self.ISPs
-
-    def set_city_name(self, city_name):
-        self.city_name = city_name
-
-    def set_ISPs(self, ISPs):
-        self.ISPs = ISPs
-
-
-class NetworkMetric:    # This class represents network performance metrics collected for a specific location.
-    def __init__(self, id, metric_name, value, timestamp, country_id, region_id, city_id):
-        self.id = id
-        self.metric_name = metric_name
-        self.value = value
-        self.timestamp = timestamp
+        self.name = name
         self.country_id = country_id
-        self.region_id = region_id
-        self.city_id = city_id
 
     def get_id(self):
         return self.id
 
-    def get_metric_name(self):
-        return self.metric_name
-
-    def get_value(self):
-        return self.value
-
-    def get_timestamp(self):
-        return self.timestamp
+    def get_name(self):
+        return self.name
 
     def get_country_id(self):
         return self.country_id
 
-    def get_region_id(self):
-        return self.region_id
-
-    def get_city_id(self):
-        return self.city_id
-
-    def set_metric_name(self, metric_name):
-        self.metric_name = metric_name
-
-    def set_value(self, value):
-        self.value = value
-
-    def set_timestamp(self, timestamp):
-        self.timestamp = timestamp
+    def set_name(self, name):
+        self.name = name
 
     def set_country_id(self, country_id):
         self.country_id = country_id
 
+
+class City:     # Represents a city within a region
+    def __init__(self, id, name, region_id):
+        self.id = id
+        self.name = name
+        self.region_id = region_id
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def get_region_id(self):
+        return self.region_id
+
+    def set_name(self, name):
+        self.name = name
+
     def set_region_id(self, region_id):
         self.region_id = region_id
 
-    def set_city_id(self, city_id):
-        self.city_id = city_id
+
+class ASN:      # Represents an Autonomous System Number
+    def __init__(self, id, asn):
+        self.id = id
+        self.asn = asn
+
+    def get_id(self):
+        return self.id
+
+    def get_asn(self):
+        return self.asn
+
+    def set_asn(self, asn):
+        self.asn = asn
+
+
+class AfricaRegion:   # Represents a region within Africa
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+
+class NetworkPerformanceDataNormalized:    # Represents network performance metrics with normalized references
+    def __init__(self, id, date, client_country_id, client_city_id, client_region_id, client_asn_id, avg_download_speed, avg_upload_speed, avg_latency, africa_region_id):
+        self.id = id
+        self.date = date
+        self.client_country_id = client_country_id
+        self.client_city_id = client_city_id
+        self.client_region_id = client_region_id
+        self.client_asn_id = client_asn_id
+        self.avg_download_speed = avg_download_speed
+        self.avg_upload_speed = avg_upload_speed
+        self.avg_latency = avg_latency
+        self.africa_region_id = africa_region_id
+
+    def get_id(self):
+        return self.id
+
+    def get_date(self):
+        return self.date
+
+    def get_client_country_id(self):
+        return self.client_country_id
+
+    def get_client_city_id(self):
+        return self.client_city_id
+
+    def get_client_region_id(self):
+        return self.client_region_id
+
+    def get_client_asn_id(self):
+        return self.client_asn_id
+
+    def get_avg_download_speed(self):
+        return self.avg_download_speed
+
+    def get_avg_upload_speed(self):
+        return self.avg_upload_speed
+
+    def get_avg_latency(self):
+        return self.avg_latency
+
+    def get_africa_region_id(self):
+        return self.africa_region_id
+
+    def set_date(self, date):
+        self.date = date
+
+    def set_client_country_id(self, client_country_id):
+        self.client_country_id = client_country_id
+
+    def set_client_city_id(self, client_city_id):
+        self.client_city_id = client_city_id
+
+    def set_client_region_id(self, client_region_id):
+        self.client_region_id = client_region_id
+
+    def set_client_asn_id(self, client_asn_id):
+        self.client_asn_id = client_asn_id
+
+    def set_avg_download_speed(self, avg_download_speed):
+        self.avg_download_speed = avg_download_speed
+
+    def set_avg_upload_speed(self, avg_upload_speed):
+        self.avg_upload_speed = avg_upload_speed
+
+    def set_avg_latency(self, avg_latency):
+        self.avg_latency = avg_latency
+
+    def set_africa_region_id(self, africa_region_id):
+        self.africa_region_id = africa_region_id
