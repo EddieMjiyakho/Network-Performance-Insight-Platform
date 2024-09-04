dd4b2892b4a39d227c480172ad3bc69d8f1b2e01
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2024-09-04 12:17:29 +0200

    fix keys dependencies

--- mlab/models.py
+++ mlab/models.py
@@ -19,9 +19,6 @@ class NetworkMetric(models.Model):
     avg_download_speed = models.FloatField()
     avg_upload_speed = models.FloatField()
     avg_latency = models.FloatField()
-    
-    class Meta:
-        unique_together = ('asn', 'avg_download_speed', 'avg_upload_speed', 'avg_latency')
 
 class AfricaRegion(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -31,24 +28,14 @@ class Country(models.Model):
     africa_region = models.ForeignKey(AfricaRegion, on_delete=models.CASCADE)
     network_metric = models.ForeignKey(NetworkMetric, on_delete=models.CASCADE)
 
-    class Meta:
-        unique_together = ('name', 'africa_region', 'network_metric')
-
 class Region(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    africa_region = models.ForeignKey(AfricaRegion, on_delete=models.CASCADE)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     network_metric = models.ForeignKey(NetworkMetric, on_delete=models.CASCADE)
 
-    class Meta:
-        unique_together = ('name', 'africa_region', 'country', 'network_metric')
 
 class City(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    africa_region = models.ForeignKey(AfricaRegion, on_delete=models.CASCADE)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    network_metric = models.ForeignKey(NetworkMetric, on_delete=models.CASCADE)
     client_region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    network_metric = models.ForeignKey(NetworkMetric, on_delete=models.CASCADE)
 
-    class Meta:
-        unique_together = ('name', 'client_region', 'country', 'africa_region', 'network_metric')
