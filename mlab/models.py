d1e76fcd5049516a85cba756dae761650fc495bd
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2024-09-11 14:17:24 +0200

    create ndt uploads and downloads tables

--- mlab/models.py
+++ mlab/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# dataset from measurement-lab.cloudflare.speedtest_speed1
 class NetworkPerformanceData(models.Model):
     date = models.DateField()
     clientCountry = models.CharField(max_length=255)
@@ -25,3 +26,39 @@ class Region(models.Model):
 
 class City(models.Model):
     city_name = models.CharField(max_length=255)
+
+# dataset from measurement-lab.ndt.unified_downloads
+class ndt_unified_downloads(models.Model):
+    date = models.DateField()
+    test_time = models.TimeField()
+    throughput = models.FloatField()  # MeanThroughputMbps
+    min_rtt = models.FloatField()  # MinRTT
+    packet_loss = models.FloatField()  # LossRate
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    accuracy_radius_km = models.FloatField()
+    isp_number = models.IntegerField()  # ASNumber
+    isp = models.CharField(max_length=255)  # ASName
+
+    def __str__(self):
+        return f"{self.date} - {self.city} ({self.isp})"
+    
+# dataset from measurement-lab.ndt.unified_uploads
+class ndt_unified_uploads(models.Model):
+    date = models.DateField()
+    test_time = models.TimeField()
+    throughput = models.FloatField()  # MeanThroughputMbps
+    min_rtt = models.FloatField()  # MinRTT
+    packet_loss = models.FloatField()  # LossRate
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    accuracy_radius_km = models.FloatField()
+    isp_number = models.IntegerField()  # ASNumber
+    isp = models.CharField(max_length=255)  # ASName
+
+    def __str__(self):
+        return f"{self.date} - {self.city} ({self.isp})"
