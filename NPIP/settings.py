931939b7eed98c4385d8638b515e2e9f1938b6eb
EddieMjiyakho <eddie.mjiyakho@gmail.com>
2025-01-31 17:42:44 +0200

    hosting

--- NPIP/settings.py
+++ NPIP/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,6 +92,10 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -127,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = 'media/' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- NPIP/urls.py
+++ NPIP/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mlab.urls')),  # Include mlab URLs, which will handle the React app and API routes
 ]
+
+urlpatterns += staticfiles_urlpatterns() 
 
 # Serve static files during development
 if settings.DEBUG:
