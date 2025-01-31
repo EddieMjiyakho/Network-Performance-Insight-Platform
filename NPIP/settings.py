0a3c1240677a9472179f9c56cab40e64d48831c8
EddieMjiyakho <eddie.mjiyakho@gmail.com>
2025-01-31 18:21:20 +0200

    test

--- NPIP/settings.py
+++ NPIP/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$zh0k0@im-9_-#^mv(u+91xgz(crd5j_l8s5oeq_muc974ih6a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
